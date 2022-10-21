af9e53a9e833bfb54632e8fb3daf6fd4f0918934
abukati <artiomb7@gmail.com>
2022-10-21 18:45:21 +0300

    Initial Commit

---
+++ shop/shopApp.swift
@@ -0,0 +1,17 @@
+//
+//  shopApp.swift
+//  shop
+//
+//  Created by Artiom Bukati on 21/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct shopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
