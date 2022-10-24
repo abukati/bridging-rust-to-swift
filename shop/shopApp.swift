cd0216d9630ddeda841c9cd5cd0308d1421de306
abukati <artiomb7@gmail.com>
2022-10-24 19:51:56 +0300

    init

--- shop/shopApp.swift
+++ shop/shopApp.swift
@@ -6,9 +6,31 @@
 //
 
 import SwiftUI
+import RustBackend
+
+struct StoreInfo {
+    var chain: String
+    var branch: String
+    var list_type: String
+    var date: String
+    var list: String
+}
+
+class ViewModel: ObservableObject {
+    
+    @Published var str = ""
+    
+    init() {
+        
+    }
+    
+}
 
 @main
 struct shopApp: App {
+    
+    let vm = ViewModel()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
